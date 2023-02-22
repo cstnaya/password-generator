a5d71a1a8fa3fec6bb8052c00b0cafb611df126f
aya <houtaitane0716@gmail.com>
2023-02-22 20:48:41 +0800

    fix vite config for github pages

--- vite.config.js
+++ vite.config.js
@@ -18,4 +18,8 @@ export default defineConfig({
       },
     },
   },
+  /** If you want to deploy site on github pages, you need to set base-url.
+   *  Learn more detail: https://vitejs.dev/guide/static-deploy.html#github-pages
+   */
+  base: "/password-generator/",
 });
